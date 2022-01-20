2435edc25c31074d4d31b7a8a10433da8c622a45
orini <ori.nizri.tal@gmail.com>
2022-01-20 16:56:03 +0200

    heroku

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const mongoose = require('./db/mongoose')
+const mongoose = require('./src/db/mongoose')
 const port = process.env.PORT || 3000;
 const cors = require('cors')
 const path = require('path')
