5baca42d44d3f0ff031c9313734d07e8fc19d04e
orini <ori.nizri.tal@gmail.com>
2022-01-21 13:01:32 +0200

    connection fix

--- routes/client.js
+++ routes/client.js
@@ -2,7 +2,7 @@ const express = require('express')
 const clientRouter = express.Router();
 const { getAllClients, addClient } = require('../controllers/client')
 
-clientRouter.get('/',addClient);
+clientRouter.get('/clients',addClient);
 // clientRouter.get('/', getAllClients);
 
 
